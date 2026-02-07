348fbbe05b3ca93ce0c9187437e9a1becdde2687
ddr1uszz-oss <ddr1uszz@gmail.com>
2026-02-07 21:12:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentineee? 💝",
+    pageTitle: "😁💝🤔",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -22,15 +22,15 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            text: "Pwede po magtanongg?",                                    // First interaction
+            yesBtn: "Ih",                                             // Text for "Yes" button
+            noBtn: "Ayoko",                                               // Text for "No" button
+            secretAnswer: "Sige na nga"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
-            nextBtn: "Next ❤️"                                         // Text for the next button
+            text: "Gaano kalaki yung tiwala mo sakin? ☺️",                          // For the love meter
+            startText: "Ganto lang",                                   // Text before the percentage
+            nextBtn: "Next Question"                                         // Text for the next button
         },
         third: {
             text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOHHHHGZGZHGZHG??!! 😱😱",  // Shows when they go past 5000%
+        high: "WEHHHHHHH???",              // Shows when they go past 1000%
+        normal: "Sige okay nayan"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
